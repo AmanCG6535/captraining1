5cb7d2aa976f8c760273735484873186af30cb2c
Amanpreet Singh <amanpreet.d.singh@capgemini.com>
2025-05-07 09:36:14 +0000

    first commit

---
+++ project1/file1.js
@@ -0,0 +1,14 @@
+function addition(a, b) {
+    console.log(`addition ${a + b} `)
+}
+function subtraction(a, b) {
+    console.log(`subtraction ${a - b} `)
+}
+function multiplication(a , b) {
+    console.log(`multiplication ${a * b} `)
+}
+function division(a, b) {
+    console.log(`division ${a / b} `)
+}
+
+module.exports = { addition , subtraction , multiplication , division }
---
+++ project1/file2.js
@@ -0,0 +1,5 @@
+const file1 = require('./file1');
+file1.addition(1,1);
+file1.subtraction(1,1);
+file1.multiplication(2,3);
+file1.division(4,2);
